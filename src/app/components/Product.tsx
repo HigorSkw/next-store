5529d887a8456396f811edd50af0b370066530d8
Higor Skowronski De Souza - External <e-skowronski@weg.net>
2023-10-27 15:36:43 -0300

    feat-stile-component-product

--- src/app/components/Product.tsx
+++ src/app/components/Product.tsx
@@ -1,4 +1,6 @@
 import { ProductType } from '@/@types/ProductType';
+import Image from 'next/image';
+import ProductImage from './ProductImagem';
 
 type ProductProps = {
   product: ProductType;
@@ -7,8 +9,11 @@ type ProductProps = {
 export const Product = ({ product }: ProductProps) => {
   return (
     <div className="flex flex-col shadow-lg h-96 bg-slate-800 p-5 text-gray-300">
-      <div className="relative max-h-72 flex-1">IMG </div>
-      <div className="flex justify-between font-bold my-3">{product.title}</div>
+      {/* <ProductImage product={product} key={product.id} fill /> */}
+      <div className="flex justify-between font-bold my-3">
+        <p className="w-40 truncate">{product.title}</p>
+        <p className="text-md text-teal-300">{product.price}</p>
+      </div>
       <button className="rounded-md bg-teal-600 text-white p-3.5 py-2.5 text-sm text-center">
         Adicionar ao Carrinho
       </button>
