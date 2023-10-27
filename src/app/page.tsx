1b50e67adde5c4c1fb84ab7e818abda82ae19fe6
Higor Skowronski De Souza - External <e-skowronski@weg.net>
2023-10-27 14:07:15 -0300

    feat- add component product

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { ProductType } from '@/@types/ProductType';
 import { database } from '@/lib/database';
+import { Product } from './components/Product';
 
 export default async function Home() {
   const products: ProductType[] = database;
@@ -10,7 +11,7 @@ export default async function Home() {
     <div className="max-w-7xl mx-auto px-8 pt-8 xl:px-0">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
         {products.map((product) => (
-          <div key={product.id}>{product.title}</div>
+          <Product key={product.id} product={product} />
         ))}
       </div>
     </div>
