8d2819a9078369995970ed430b322af9685f521d
Higor Skowronski De Souza - External <e-skowronski@weg.net>
2023-10-27 13:53:59 -0300

    feat- type about product

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,17 @@
-import Image from 'next/image';
+import { ProductType } from '@/@types/ProductType';
+import { database } from '@/lib/database';
 
-export default function Home() {
+export default async function Home() {
+  const products: ProductType[] = database;
+
   return (
     // Segunda opção seria o className - "container"
+
     <div className="max-w-7xl mx-auto px-8 pt-8 xl:px-0">
-      <div
-        className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6
-      ">
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-          Prod 1
-        </div>
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-          Prod 2
-        </div>
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-          Prod 3
-        </div>
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-          Prod 4
-        </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+        {products.map((product) => (
+          <div key={product.id}>{product.title}</div>
+        ))}
       </div>
     </div>
   );
